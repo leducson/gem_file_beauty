e630cbee503edd210fd78dd0ad608dc1d3a32e5a
Le Duc Son <le.duc.son@sun-asterisk.com>
2020-05-14 13:05:49 +0700

    Init

---
+++ lib/gem_file_beauty/version.rb
@@ -0,0 +1,3 @@
+module GemFileBeauty
+  VERSION = "0.1.1"
+end
---
+++ spec/gem_file_beauty_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GemFileBeauty do
+  it "has a version number" do
+    expect(GemFileBeauty::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
